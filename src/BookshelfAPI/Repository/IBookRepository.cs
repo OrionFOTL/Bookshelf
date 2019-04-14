f6bff7f6ed5aed646e5a3e57d9eed204e175df93
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-04-14 19:56:15 +0200

    added book repository

---
+++ src/BookshelfAPI/Repository/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookshelfAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookshelfAPI.Repository
+{
+    public interface IBookRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Get(long id);
+        Task Add(TEntity entity);
+        Task Update(Book book, TEntity entity);
+        Task Delete(Book book);
+    }
+}

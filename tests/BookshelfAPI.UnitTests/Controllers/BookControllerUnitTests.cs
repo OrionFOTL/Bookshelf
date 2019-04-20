441355bcaf82bb539d8a8c69e9d3be137310a8d2
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-04-20 13:35:53 +0200

    added more unit tests

--- tests/BookshelfAPI.UnitTests/Controllers/BookControllerUnitTests.cs
+++ tests/BookshelfAPI.UnitTests/Controllers/BookControllerUnitTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -85,5 +86,149 @@ namespace BookshelfAPI.UnitTests.Controllers
             Assert.Equal(StatusCodes.Status201Created, response.StatusCode);
             Assert.Equal(expectedTitle, bookreceived.Title);
         }
+        
+        [Fact]
+        public async Task post_new_book_on_existing_id()
+        {
+            // Arrange
+            var repository = BookContextMocker.GetInMemoryBookRepository(nameof(post_new_book_on_existing_id));
+            var controller = new BookController(repository);
+            var bookToInsert = new Book()
+            {
+                Id = 1,
+                Title = "Ender's Game",
+                Author = "Orson Scott Card",
+                ISBN = "312159785",
+                Year = 1985
+            };
+            var expectedMessage = "Book with specified ID already exists.";
+
+            // Act
+            var response = await controller.Post(bookToInsert) as ObjectResult;
+
+            // Assert
+            Assert.Equal(StatusCodes.Status409Conflict, response.StatusCode);
+            Assert.Equal(expectedMessage, response.Value);
+        }
+
+        [Fact]
+        public async Task update_existing_book()
+        {
+            // Arrange
+            var repository = BookContextMocker.GetInMemoryBookRepository(nameof(update_existing_book));
+            var controller = new BookController(repository);
+            var updatedBook = new Book()
+            {
+                Title = "Ender's Game",
+                Author = "Orson Scott Card",
+                ISBN = "312159785",
+                Year = 1985
+            };
+
+            // Act
+            var response = await controller.Put(1, updatedBook);
+
+            // Assert
+            Assert.IsType<OkResult>(response);
+        }
+
+        [Fact]
+        public async Task update_nonexisting_id()
+        {
+            // Arrange
+            var repository = BookContextMocker.GetInMemoryBookRepository(nameof(update_nonexisting_id));
+            var controller = new BookController(repository);
+            var updatedBook = new Book()
+            {
+                Title = "Ender's Game",
+                Author = "Orson Scott Card",
+                ISBN = "312159785",
+                Year = 1985
+            };
+            var expectedMessage = "The book to update record couldn't be found.";
+
+            // Act
+            var response = await controller.Put(999, updatedBook) as ObjectResult;
+
+            // Assert
+            Assert.Equal(StatusCodes.Status404NotFound, response.StatusCode);
+            Assert.Equal(expectedMessage, response.Value);
+        }
+
+        [Fact]
+        public async Task delete_existing_book()
+        {
+            // Arrange
+            var repository = BookContextMocker.GetInMemoryBookRepository(nameof(delete_existing_book));
+            var controller = new BookController(repository);
+
+            // Act
+            var response = await controller.Delete(3);
+            var getBooks = await controller.GetAll() as ObjectResult;
+            var books = getBooks.Value as List<Book>;
+
+            // Assert
+            Assert.IsType<OkResult>(response);
+            Assert.Equal(2, books.Count);
+        }
+
+        [Fact]
+        public async Task delete_nonexisting_book()
+        {
+            // Arrange
+            var repository = BookContextMocker.GetInMemoryBookRepository(nameof(delete_nonexisting_book));
+            var controller = new BookController(repository);
+            var expectedMessage = "The book to delete record couldn't be found.";
+
+            // Act
+            var response = await controller.Delete(999) as ObjectResult;
+            var getBooks = await controller.GetAll() as ObjectResult;
+            var books = getBooks.Value as List<Book>;
+
+            // Assert
+            Assert.Equal(StatusCodes.Status404NotFound, response.StatusCode);
+            Assert.Equal(expectedMessage, response.Value);
+            Assert.Equal(3, books.Count);
+        }
+
+        // Dekorator [ApiController] w BookController.cs sam sprawdza czy podano wszystkie
+        // wymagane pola w żądaniu i zwraca kod 400, ale ta walidacja nie jest aktywna w testach;
+        // Test sprawdza samą walidację
+        [Fact]
+        public async Task validate_incomplete_book()
+        {
+            // Arrange
+            var obj = new Book()
+            {
+                Year = 1956
+            };
+            var context = new ValidationContext(obj);
+            var results = new List<ValidationResult>();
+
+            // Act 
+            var valid = Validator.TryValidateObject(obj, context, results, true);
+
+            // Assert
+            Assert.False(valid);
+        }
+
+        //[Fact]
+        //public async Task update_book_without_required_data()
+        //
+        //{
+        //    // Arrange
+        //    var repository = BookContextMocker.GetInMemoryBookRepository(nameof(update_book_without_required_data));
+        //    var controller = new BookController(repository);
+        //    var updatedBook = new Book()
+        //    {
+        //        Year = 1985
+        //    };
+
+        //    // Act
+        //    var response = await controller.Put(1, updatedBook) as ObjectResult;
+
+        //    // Assert
+        //    Assert.Equal(StatusCodes.Status400BadRequest, response.StatusCode);
+        //}
     }
 }
